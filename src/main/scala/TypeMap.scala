409b44e2e3c19abaddf3ce4101692fdeacf3f799
kraktus <kraktus@users.noreply.github.com>
2024-07-20 21:23:25 +0200

    refactor

--- src/main/scala/TypeMap.scala
+++ src/main/scala/TypeMap.scala
@@ -1,10 +1,12 @@
 import java.util.concurrent.ConcurrentHashMap as CMap
 import scala.quoted.*
 
+type Backend[V] = CMap[String, V]
+
+
 // associate a value of type V to each type in the tuple K
 // TODO use PHF
-class TypeMap[K <: Tuple, V](private val map: CMap[String, V]):
-  inline def keys = typeNamesTupleMacro[K] // Obviously not known here because K is generic
+class TypeMap[K <: Tuple, V](private val map: Backend[V]):
 
   inline def get[T]: Option[V]      = ${ getImpl[T, K, V]('map) }
   inline def put[T](value: V): Unit = ${ putImpl[T, K, V]('map, 'value) }
@@ -12,17 +14,17 @@ class TypeMap[K <: Tuple, V](private val map: CMap[String, V]):
 def getImpl[T, K <: Tuple, V](
     map: Expr[CMap[String, V]]
 )(using Quotes, Type[T], Type[K], Type[V]): Expr[Option[V]] =
-  opImpl[T, K, V, Option[V]](map, '{ Option(${ map }.get(${ Expr(Type.show[T]) })) })
+  opImpl[T, K, V, Option[V]](map, '{ Option(${ map }.get(${ typeNameImpl[T] })) })
 
-def putImpl[T, K <: Tuple, V](map: Expr[CMap[String, V]], value: Expr[V])(using
+def putImpl[T, K <: Tuple, V](map: Expr[Backend[V]], value: Expr[V])(using
     Quotes,
     Type[T],
     Type[K],
     Type[V]
 ): Expr[Unit] =
-  opImpl[T, K, V, Unit](map, '{ ${ map }.put(${ Expr(Type.show[T]) }, ${ value }) })
+  opImpl[T, K, V, Unit](map, '{ ${ map }.put(${ typeNameImpl[T] }, ${ value }) })
 
-def opImpl[T, K <: Tuple, V, Result](map: Expr[CMap[String, V]], res: Expr[Result])(using
+def opImpl[T, K <: Tuple, V, Result](map: Expr[Backend[V]], res: Expr[Result])(using
     Quotes,
     Type[T],
     Type[K],
@@ -34,4 +36,4 @@ def opImpl[T, K <: Tuple, V, Result](map: Expr[CMap[String, V]], res: Expr[Resul
     case Expr(false) => report.errorAndAbort(s"Type ${Type.show[T]} not found in tuple ${Type.show[K]}")
 
 object TypeMap:
-  def empty[K <: Tuple, V] = TypeMap[K, V](new CMap())
+  def empty[K <: Tuple, V] = TypeMap[K, V](new Backend())
